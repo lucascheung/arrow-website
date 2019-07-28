31594f9c8a3489479b334f2e60bd1c3618c20232
Lucas Cheung <lucascheungls@gmail.com>
2019-07-28 17:45:15 +0100

    dropdown

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -7,9 +7,13 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
     document.querySelector(".navbar-wagon-brand img").style.width = "70px";
+    document.querySelector(".navbar-wagon-brand-small img").style.width = "70px";
+    document.querySelector(".dropdown-toggle").style.padding = "20px";
     document.querySelector('.navbar-wagon').style.backgroundColor = "#111116";
   } else {
     document.querySelector(".navbar-wagon-brand img").style.width = "100px";
+    document.querySelector(".navbar-wagon-brand-small img").style.width = "100px";
+        document.querySelector(".dropdown-toggle").style.padding = "35px";
     document.querySelector('.navbar-wagon').style.backgroundColor = 'transparent'
   }
 }
