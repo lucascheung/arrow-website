a0329517bad19643dc40755dcd7e3ce1aba7fd5e
Lucas Cheung <lucascheungls@gmail.com>
2019-07-30 00:36:28 +0100

    add animation

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -5,15 +5,47 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
+  addClasses();
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
     document.querySelector(".navbar-wagon-brand img").style.width = "70px";
     document.querySelector(".navbar-wagon-brand-small img").style.width = "70px";
     document.querySelector(".dropdown-toggle").style.padding = "20px";
-    document.querySelector('.navbar-wagon').style.backgroundColor = "#111116";
+    document.querySelector('.navbar-wagon').style.backgroundColor = "rgba(0,0,0,0.8)";
+    document.querySelector('.navbar-wagon').style.height = "50px";
   } else {
     document.querySelector(".navbar-wagon-brand img").style.width = "100px";
     document.querySelector(".navbar-wagon-brand-small img").style.width = "100px";
-        document.querySelector(".dropdown-toggle").style.padding = "35px";
-    document.querySelector('.navbar-wagon').style.backgroundColor = 'transparent'
+    document.querySelector(".dropdown-toggle").style.padding = "35px";
+    document.querySelector('.navbar-wagon').style.backgroundColor = 'transparent';
+    document.querySelector('.navbar-wagon').style.height = "100px";
   }
+}
+
+
+function addClasses() {
+  const intro = document.querySelector('.intro-container');
+  const about = document.querySelector('.about-content');
+  const service = document.querySelector('.service-content');
+  if (isScrolledIntoView(intro)) {
+    intro.classList.remove('hidden');
+    intro.classList.add('fadeIn');
+  }
+  if (isScrolledIntoView(about)) {
+    about.classList.remove('hidden');
+    about.classList.add('slideLeft');
+  }
+  if (isScrolledIntoView(service)) {
+    service.classList.remove('hidden');
+    service.classList.add('slideRight');
+  }
+}
+
+function isScrolledIntoView(elem) {
+  var docViewTop = $(window).scrollTop();
+  var docViewBottom = docViewTop + $(window).height();
+
+  var elemTop = $(elem).offset().top;
+  var elemBottom = elemTop + $(elem).height();
+
+  return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
 }
